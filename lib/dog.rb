a01d7d8bda1768fbd617c9bd3f487d98334b9cf6
Meghan Flynn <mflynn333@gmail.com>
2019-12-28 19:03:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,16 @@
 class Dog 
   
-  def initialize(breed = "Mutt")
+  def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed 
+  end 
+  
+  def name=(name)
+    @name = name 
+  end 
+  
+  def name 
+    @name 
   end 
   
   def breed=(breed)
